37937aee953a28cf92c870ee5058a938847123a5
wadsahin <wadsahinalom@gmail.com>
2024-12-23 21:58:06 +0600

    Recent query section completed

--- src/components/RecentQueryCard.jsx
+++ src/components/RecentQueryCard.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 
-const MyQueryCard = ({ query }) => {
-  const {prod_name, prod_brand, prod_image, query_title, reason, user_name, user_email, user_photo, createdAt, recommendationCount} = query;
-  
-  // const {prod_name, prod_brand, prod_image, query_title, reason, user_name, user_email, user_photo, createdAt, recommendationCount} = query;
+const RecentQueryCard = ({ query }) => {
+  const { prod_name, prod_brand, prod_image, query_title, reason, user_name, user_email, user_photo, createdAt } = query;
+  // customize creation time
+  const date = new Date(createdAt);
+  const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+  const formattedDate = date.toLocaleDateString('en-US', options);
 
   return (
     <div className="card card-compact bg-base-100 shadow-xl">
       <figure>
         <img
+          className="h-[300px] object-cover"
           src={prod_image} />
       </figure>
       <div className="card-body">
+        <div className="flex items-center gap-2">
+          <img className="w-12 h-12 rounded-full" src={user_photo} alt="" />
+          <h4 className="bg-gray-300 px-2 rounded-sm font-medium">{user_name}</h4>
+          <strong>{formattedDate}</strong>
+        </div>
         <h2 className="card-title">{prod_name}</h2>
         <p><span className="text-base font-medium">Brand: </span> <span className="font-mono">{prod_brand}</span></p>
         <h3 className="text-xl font-semibold font-serif">{query_title}</h3>
         <p><span className="text-base font-medium">Boycotting reason:</span> {reason}</p>
         <div className="card-actions justify-end mt-5">
           <button className="btn bg-teal-600 text-white">View Details</button>
-          <button className="btn btn-warning">Update</button>
-          <button className="btn btn-error text-white">Delete</button>
         </div>
       </div>
     </div>
   );
 };
 
-export default MyQueryCard;
+export default RecentQueryCard;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from "react";
 import Carousel from "./Carousel";
+import axios from "axios";
+import RecentQueryCard from "../../components/RecentQueryCard";
 
 const Home = () => {
+  const [queries, setQueries] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/recent-queries")
+      .then(res => {
+        // console.log(res.data);
+        setQueries(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [])
   return (
     <div>
       {/* Slider */}
       <Carousel />
-      Home
+      <div className="w-11/12 lg:w-10/12 mx-auto">
+        <div className="mb-10">
+          <h2 className="text-center mt-5 text-4xl font-semibold">Recent Queries</h2>
+          <p className="border-b-2 border-b-orange-500 w-72 mx-auto mt-3"></p>
+        </div>
+
+        {
+          queries.length ?
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+              {
+                queries.map(query => <RecentQueryCard key={query._id} query={query} />)
+              }
+            </div>
+
+            : <div>
+              No data available!!
+            </div>
+        }
+
+
+      </div>
     </div>
   );
 };
