2a50fa9e431d2ceaeef175920913c166d2d8e853
wadsahin <wadsahinalom@gmail.com>
2024-12-24 22:08:10 +0600

    Readme.me file added with contents

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
-# React + Vite
+# Welcome to my 'TrendPicks'
+***
+## Project Purpose:
+**Our TrendPicks a Product Recommendation platform for recommending products . Where users can**
+- Users can Add/update/Delete their own Query  about a product in The System
+- Users can view other queries for alternative products on the  Websites and see the - Details and other recommendations also.
+- Users can Add recommendations also for certain products. 
+- Users can delete their recommendations.
+- Users can Modify / Delete their comments also. 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+### Key Features of this project:
+- **Uniqe & Responsive Design**
+- **CRUD Operation**
+- **Query Details**
+- **Authentication**
+- **Delete Functionality**
+- **Update Features**
+- **Authenticated user management**
+- **Dynamic Routing**
+- **Some Action validation(password, delete)**
+- **Conditional Rendering**
+- **Eye-catching & Beautifull animated sweet messages**
+### Used npm packages:
+- **React-router-dom**
+- **axios**
+- **React-icons**
+- **sweetalert2**
+- **swiper silder**
+- **firebase**
 
-Currently, two official plugins are available:
+### 4. Live Link:
+```
+https://bpl-2024-dream-team-squad.netlify.app/
+```
+[Click to live](https://bpl-2024-dream-team-squad.netlify.app/)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/pages/recommendation/MyRecommendations.jsx
+++ src/pages/recommendation/MyRecommendations.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../hook/useAuth";
+import Swal from "sweetalert2";
 
 const MyRecommendations = () => {
   const [recommendations, setRecommendations] = useState([]);
@@ -17,16 +18,65 @@ const MyRecommendations = () => {
       })
   }, [user]);
 
-  const handleDelete = (rec_id, queryId) =>{
-    console.log({rec_id, queryId})
+  const handleDelete = (rec_id, queryId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Delete"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/my-recommendation-delete/${rec_id}`, {
+          method: "DELETE",
+        })
+          .then(res => res.json())
+          .then(data => {
+            // console.log(data);
+            if (data.deletedCount > 0) {
+              const remaining = recommendations.filter(rec => rec._id !== rec_id);
+              setRecommendations(remaining);
+
+              // Decreasing recommendation count
+              fetch(`http://localhost:5000/recommendation-decrease/${queryId}`, {
+                method: "PUT",
+                headers: {
+                  "content-type": "application/json"
+                }
+              })
+              .then(res => res.json())
+              .then(data => {
+                console.log(data);
+                if(data.modifiedCount > 0){
+                  Swal.fire({
+                    title: "Deleted & Decreased the count",
+                    text: "Your recommendation has been deleted and decreased recommendation count",
+                    icon: "success"
+                  });
+                }
+              })
+
+              
+            }
+          })
+
+      }
+    });
+
+
+    // console.log({rec_id, queryId})
   }
+
+
   return (
     <div className="overflow-x-auto w-11/12 md:w-10/12 mx-auto">
       <table className="table">
         {/* head */}
         <thead>
           <tr>
-            <th>Recommendation Details</th> 
+            <th>Recommendation Details</th>
             <th>Reason</th>
             <th>Query Details</th>
             <th>Action</th>
