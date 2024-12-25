dc1b82be1803db57555cbe0fdad24baea14f66bc
wadsahin <wadsahinalom@gmail.com>
2024-12-25 21:26:00 +0600

    Fix some bugs & Updated the layout change functionality

--- src/pages/query/Queries.jsx
+++ src/pages/query/Queries.jsx
@@ -7,18 +7,19 @@ import { useState } from "react";
 const Queries = () => {
   const [layout, setLayout] = useState(0);
   const queries = useLoaderData();
+  // console.log(layout);
 
   // Handle One Column Layout
-  const handleOneColumnLayout = () =>{
+  const handleOneColumnLayout = () => {
     setLayout(1);
   }
   // Handle Two Column Layout
-  const handleTwoColumnLayout = () =>{
+  const handleTwoColumnLayout = () => {
     setLayout(2);
   }
 
   // Handle Three Column Layout
-  const handleThreeColumnLayout = () =>{
+  const handleThreeColumnLayout = () => {
     setLayout(3);
   }
   return (
@@ -45,10 +46,11 @@ const Queries = () => {
           <button onClick={handleOneColumnLayout}><CiGrid2H size={28} /></button>
           <button onClick={handleTwoColumnLayout}><CiGrid2V size={28} /></button>
           <button className="hidden lg:flex" onClick={handleThreeColumnLayout}><IoGridOutline size={28} /></button>
-          
+
         </div>
       </div>
-      <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 my-10 ${layout == 1 && 'md:grid-cols-1 md:max-w-2xl md:mx-auto lg:grid-cols-1 lg:max-w-3xl lg:mx-auto' || layout == 2 && 'md:grid-cols-2 lg:grid-cols-2' || layout == 3 && 'lg:grid-cols-3'} `}>
+      <hr className="border-teal-500" />
+      <div className={`grid gap-8 my-10 ${layout == 0 && "grid-cols-1 md:grid-cols-2 lg:grid-cols-3" || layout == 1 && "grid-cols-1 md:max-w-2xl lg:max-w-3xl md:mx-auto" || layout == 2 && "grid-cols-1 md:grid-cols-2" || layout == 3 && "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"}`}>
         {
           queries.map(query => <QueriesCard key={query._id} query={query} />)
         }
