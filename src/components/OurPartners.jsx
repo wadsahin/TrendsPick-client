b673b6756a464043ad983138f92cf64781601226
wadsahin <wadsahinalom@gmail.com>
2024-12-25 18:41:35 +0600

    Parthers section & Change Layout feature completed

--- src/components/OurPartners.jsx
+++ src/components/OurPartners.jsx
@@ -1,12 +1,70 @@
+import { Link } from "react-router-dom";
+import partner_0 from "../../src/assets/partners/partners_0.jpeg";
+import partner_1 from "../../src/assets/partners/partners_1.png";
+import partner_2 from "../../src/assets/partners/partners_2.jpeg";
+import partner_3 from "../../src/assets/partners/partners_3.jpeg";
+import partner_4 from "../../src/assets/partners/partners_4.jpg";
+import partner_5 from "../../src/assets/partners/partners_5.jpeg";
+import partner_6 from "../../src/assets/partners/partners_6.jpeg";
+import partner_7 from "../../src/assets/partners/partners_7.jpeg";
+import { RiExternalLinkLine } from "react-icons/ri";
 
 const OurPartners = () => {
   return (
     <div>
       <h2 className="text-3xl font-semibold mb-1">Our Partners</h2>
-      <p className="text-base text-gray-500">Working with those companies driving the future of security operations</p>
+      <p className="text-base text-gray-500">Working with those companies driving the future of query & recommendation operations</p>
       <div className="border border-teal-600 mt-3"></div>
-      <div>
+      <div className="mt-8 flex flex-wrap gap-10 md:w-9/12 mx-auto">
         
+        {/* partner-1 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_0} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://www.programming-hero.com/", "_blank")}>Programming Hero <RiExternalLinkLine size={22} /></Link>
+        </div>
+
+        {/* partner-2 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_1} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://brandviserpro.com/", "_blank")}>Brandviser <RiExternalLinkLine size={22} /></Link>
+        </div>
+
+        {/* partner-3 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_2} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://www.imbdagency.com/", "_blank")}>IMBD Agency Ltd. <RiExternalLinkLine size={22} /></Link>
+        </div>
+
+        {/* partner-4 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_3} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://hypedhaka.com/", "_blank")}>HYPE Dhaka <RiExternalLinkLine size={22} /></Link>
+        </div>
+
+        {/* partner-5 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_4} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://www.bizcope.com/", "_blank")}>Bizcope <RiExternalLinkLine size={22} /></Link>
+        </div>
+
+        {/* partner-6 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_5} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://www.datazoinfotech.com.bd/", "_blank")}>Datazo Info Tech <RiExternalLinkLine size={22} /></Link>
+        </div>
+
+        {/* partner-7 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_6} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://bjitgroup.com/", "_blank")}>BJIT Group <RiExternalLinkLine size={22} /></Link>
+        </div>
+
+        {/* partner-8 */}
+        <div className="border p-2 rounded-sm hover:scale-110 hover:duration-300">
+          <img src={partner_7} className="rounded-md" alt="" />
+          <Link className="text-lg font-medium text-gray-500 flex items-center gap-2" onClick={()=> window.open("https://microdeft.com/", "_blank")}>Microdeft <RiExternalLinkLine size={22} /></Link>
+        </div>
+
       </div>
     </div>
   );
--- src/pages/query/Queries.jsx
+++ src/pages/query/Queries.jsx
@@ -1,11 +1,54 @@
 import { useLoaderData } from "react-router-dom";
 import QueriesCard from "../../components/QueriesCard";
+import { CiGrid2H, CiGrid2V } from "react-icons/ci";
+import { IoGridOutline } from "react-icons/io5";
+import { useState } from "react";
 
 const Queries = () => {
+  const [layout, setLayout] = useState(0);
   const queries = useLoaderData();
+
+  // Handle One Column Layout
+  const handleOneColumnLayout = () =>{
+    setLayout(1);
+  }
+  // Handle Two Column Layout
+  const handleTwoColumnLayout = () =>{
+    setLayout(2);
+  }
+
+  // Handle Three Column Layout
+  const handleThreeColumnLayout = () =>{
+    setLayout(3);
+  }
   return (
     <div className="w-11/12 lg:w-10/12 mx-auto ">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
+      <div className="py-5 flex justify-between gap-5">
+        {/* Search part */}
+        <div>
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70">
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd" />
+            </svg>
+          </label>
+        </div>
+        {/* Toggle layout part */}
+        <div className="hidden md:flex items-center gap-2">
+          <button onClick={handleOneColumnLayout}><CiGrid2H size={28} /></button>
+          <button onClick={handleTwoColumnLayout}><CiGrid2V size={28} /></button>
+          <button className="hidden lg:flex" onClick={handleThreeColumnLayout}><IoGridOutline size={28} /></button>
+          
+        </div>
+      </div>
+      <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 my-10 ${layout == 1 && 'md:grid-cols-1 md:max-w-2xl md:mx-auto lg:grid-cols-1 lg:max-w-3xl lg:mx-auto' || layout == 2 && 'md:grid-cols-2 lg:grid-cols-2' || layout == 3 && 'lg:grid-cols-3'} `}>
         {
           queries.map(query => <QueriesCard key={query._id} query={query} />)
         }
