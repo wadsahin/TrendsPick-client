e828ce48e6b1708855834be714c05fbe5f6146be
wadsahin <wadsahinalom@gmail.com>
2025-02-12 15:45:34 +0600

    Project poolishing completed

--- src/pages/common/Footer.jsx
+++ src/pages/common/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
       </aside>
       <nav>
         <div className="grid grid-flow-col gap-4">
-          <a>
+          <a href="https://www.facebook.com" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -26,10 +26,10 @@ const Footer = () => {
                 d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
             </svg>
           </a>
-          <a>
+          <a href="https://www.linkedin.com" target="_blank">
             <BsLinkedin color="white" size={24} />
           </a>
-          <a>
+          <a href="https://www.instagram.com" target="_blank">
             <GrInstagram color="white" size={24} />
           </a>
         </div>
